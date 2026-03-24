1063712fcba480bff638185ca4fd42950060d87f
Alejandro Alfonso Yero <alejandro@Alejandros-MacBook-Pro.local>
2026-03-24 12:10:53 -0400

    fix proto issues

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut config = prost_build::Config::new();
     config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+    config.protoc_arg("--experimental_allow_proto3_optional");
     config.compile_protos(
         &[
             "proto/namespace/types.proto",
